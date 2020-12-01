f6ac3317b6e1a34cf2c3612a283342bbfdf32324
Buer Huang <buer2202@163.com>
2020-12-01 12:04:55 +0800

    数据库连接改为配置文件

--- models/models.go
+++ models/models.go
@@ -1,14 +1,19 @@
 package models
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql" // mysql
 )
 
 func init() {
-	orm.Debug = true
+	// orm.Debug = true
+	dataSource := beego.AppConfig.String("mysqluser") + ":" + beego.AppConfig.String("mysqlpass")
+	dataSource += "@tcp(" + beego.AppConfig.String("mysqlurls") + ")"
+	dataSource += "/" + beego.AppConfig.String("mysqldb") + "?charset=utf8"
+
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(10.0.31.254:3306)/party?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", dataSource)
 
 	// 需要在init中注册定义的model
 	orm.RegisterModel(
